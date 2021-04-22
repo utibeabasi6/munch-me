82377774d52ba0a53afa7480b82cf9d3eda518da
utibeabasi6 <utibeabasiumanah6@gmail.com>
2021-04-22 15:51:37 +0100

    made some ui changes

--- munchme-frontend/src/components/Contact.js
+++ munchme-frontend/src/components/Contact.js
@@ -4,22 +4,18 @@ import Hero from './Hero'
 export default function Contact() {
   return (<>
     <Hero image={'url(/top-view-of-chocolate-cake-with-raspberry-coulis.jpg)'} height='50vh' />
-    <Grid textAlign='center' stackable columns={1}>
-      <Grid.Column>
-        <Segment basic>
-          <List>
-            <List.Item icon='users' content='Utibeabasi Umanah' />
-            <List.Item icon='marker' content='Nigeria' />
-            <List.Item
-              icon='mail'
-              content={<a href='mailto:utibeabasiumanah6@gmail.com'>utibeabasiumanah6@gmail.com</a>}
-            />
-            <List.Item
-              icon='linkify'
-              content={<a href='https://portfolio-3c6e1.web.app/'>Utibeabasi Umanah</a>}
-            />
-          </List></Segment>
-      </Grid.Column>
-    </Grid></>
+    <Segment basic>
+      <List>
+        <List.Item icon='users' content='Utibeabasi Umanah' />
+        <List.Item icon='marker' content='Nigeria' />
+        <List.Item
+          icon='mail'
+          content={<a href='mailto:utibeabasiumanah6@gmail.com'>utibeabasiumanah6@gmail.com</a>}
+        />
+        <List.Item
+          icon='linkify'
+          content={<a href='https://portfolio-3c6e1.web.app/'>Utibeabasi Umanah</a>}
+        />
+      </List></Segment></>
   )
 }
