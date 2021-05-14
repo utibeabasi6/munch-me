2e43a01e69ee14fb004c8e5b037a949ed6c624ee
utibeabasi6 <utibeabasiumanah6@gmail.com>
2021-05-14 12:11:31 +0100

    New backend

--- munchme/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import CakeView, CakeDetail
-
-app_name = 'api'
-urlpatterns = [
-    path('cakes/', CakeView.as_view()),
-    path('cakes/<str:id>', CakeDetail.as_view())
-
-]
