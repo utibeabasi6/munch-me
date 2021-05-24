dad25168b8b5fe6ea6994f0479913cc9d2d15cd9
utibeabasi6 <utibeabasiumanah6@gmail.com>
2021-05-24 10:55:31 +0100

    update

--- munchme-frontend/src/pages/loginpage.js
+++ munchme-frontend/src/pages/loginpage.js
@@ -60,7 +60,7 @@ export function LoginPage() {
                     </Segment>
                 </Form>
                 <Message>
-                    New to us? <a href='#'>Sign Up</a>
+                    New to us? <a href='/sign-up'>Sign Up</a>
                 </Message>
             </Grid.Column>
         </Grid></>
